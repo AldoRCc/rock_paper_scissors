f9fed19ce7e09f253cb945cf0d1bbb5f1399f0f5
AldoRCc <aldorodarte1@gmail.com>
2025-05-05 18:40:47 -0600

    Added the logic to play 5 rounds Optimized the code removing unnecessary variables and values

--- rps.js
+++ rps.js
@@ -1,29 +1,29 @@
-let computerRandom = (Math.floor(Math.random()*3))
-let humanChoice = prompt("ROCK, PAPER, SCISSORS. CHOOSE!!!");
-humanChoice = humanChoice.toLowerCase();
+let computerRandom;
+let result;
 
 function getHumanChoice(){
-    if (humanChoice == "rock"){
+    let choice = prompt("ROCK, PAPER, SCISSORS. CHOOSE!!!");
+    choice = choice.toLowerCase();
+    if (choice == "rock"){
         console.log("Player chose rock!!!");
-        return humanChoice = 0;
+        return result = 0;
     }
-    else if (humanChoice == "paper"){
+    else if (choice == "paper"){
         console.log("Player chose paper!!!")
-        return humanChoice = 1;
+        return result = 1;
     }
-    else if (humanChoice == "scissors"){
+    else if (choice == "scissors"){
         console.log("Player chose scissors!!!")
-        return humanChoice = 2;
+        return result = 2;
     }
     else{
         console.log("Not a valid choice :(")
-        return humanChoice = 3;
+        return result = 3;
     }
 }
-getHumanChoice();
 
-let computerChoice;
 function getComputerChoice(){
+    let computerRandom = (Math.floor(Math.random()*3))
     if (computerRandom == 0){
         console.log("Computer chose rock!!!");
         return computerChoice = 0;
@@ -37,26 +37,17 @@ function getComputerChoice(){
         return computerChoice = 2;
     }
 }
-getComputerChoice(computerRandom);
 
 let humanScore = 0;
 let computerScore = 0;
 
 
-function playRound(humanChoice, computerChoice){
-    if(humanChoice == 0 && computerChoice == 1){
+function playRound(result, computerChoice){
+    if(result == 0 && computerChoice == 1 || result == 1 && computerChoice == 2 || result == 2 && computerChoice == 0){
         computerScore += 1;
         console.log("Computer won, score +1!!")
     }
-    else if (humanChoice == 1 && computerChoice == 2){
-        computerScore += 1;
-        console.log("Computer won, score +1!!")
-    }
-    else if (humanChoice == 2 && computerChoice == 0){
-        computerScore += 1;
-        console.log("Computer won, score +1!!")
-    }
-    else if (humanChoice === computerChoice){
+    else if (result === computerChoice){
         console.log ("Draw")
     }
     else{
@@ -65,7 +56,21 @@ function playRound(humanChoice, computerChoice){
     }
 }
 
-playRound(humanChoice, computerChoice)
+for(let i = 0; i < 5; i++){
+    let humanChoice = getHumanChoice();
+    let computerChoice = getComputerChoice();
+    playRound(humanChoice, computerChoice)
+}
 
+console.log("Computer final score: " + computerScore)
+console.log("Human final score: " + humanScore)
 
-console.log (computerScore)
+if(humanScore > computerScore){
+    console.log("Human won!!!")
+}
+else if(computerScore > humanScore){
+    console.log("Computer won!!!")
+}
+else {
+    console.log("DRAW!!!")
+}
